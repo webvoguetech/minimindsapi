80d865f67c97f9e8ccb085267ba72174d7262832
mallikharjun073 <webvoguetechnologies@gmail.com>
2023-12-26 20:25:22 +0530

    updated quiz level
    updated quiz level

--- models/Quiz.js
+++ models/Quiz.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const quizSchema = new mongoose.Schema({
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
   question: String,
+  level:{ type:String, default:'1'},
   options: [String],
   correctAnswer: String,
   imageUrl: String,
